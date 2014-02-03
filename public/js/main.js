018951f1501bb988fd72ba21e128c9e788a5014c
Chunyin Li <Chunyin_Li@epam.com>
2014-02-02 21:59:32 -0500

    Add breakpoints for main navigation and header. Add base js structure and js for main navigation.

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,34 @@
 
 
-function foodController ($scope) {
-    $scope.data = {
-        foods : [{
-            origin  : 'china',
-            name    : 'roast pork'
-        }, {
-            origin  : 'indian',
-            name    : 'curry chicken'
-        }]
+(function(window, document, $, _, undefined) {
+
+    RPD = this;
+
+    // Main Navigation module
+    RPD.mainNav = {
+        initialize : function () {
+            // constants
+            this.$el = $('[data-site-nav]');
+            this.$siteNavToggle = $('[data-site-nav-toggle]');
+            this.$target = this.$el.add(this.$siteNavToggle);
+            this.activeClass = 'active';
+
+            this.bindEvents();
+        },
+        bindEvents : function () {
+            this.$siteNavToggle.on('click', _.bind(this.toggleNavigation, this));
+        },
+        toggleNavigation : function () {
+            console.log('toggle nav');
+            this.$target.toggleClass(this.activeClass);
+        }
     };
 
-    $scope.addFood = function() {
-        $scope.data.foods.push({
-            origin  : $scope.newFoodOrigin,
-            name    : $scope.newFoodName
-        });
-        $scope.newFoodOrigin = '';
-        $scope.newFoodName = '';
-        console.log($scope.data);
-    }
+    RPD.initialize = function () {
+        // initialize all modules here
+        this.mainNav.initialize();
+    };
 
+    RPD.initialize();
 
-};
+}).call(window.RPD = window.RPD || {}, window, document, jQuery, _)
